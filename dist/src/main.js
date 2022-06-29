49f0310cb5e00393e952d5d35f870f7b0112c73a
masih-mehrabi <masih.mehrabi1996@gmail.com>
2022-06-29 16:49:45 +0200

    maybe final2

--- dist/src/main.js
+++ dist/src/main.js
@@ -13,7 +13,7 @@ async function bootstrap() {
         origin: '*',
         methods: 'GET, POST, PATCH, DELETE'
     });
-    await app.listen(3000);
+    await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
